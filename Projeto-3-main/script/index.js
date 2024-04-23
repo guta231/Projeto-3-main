170d4e25aacc078ee0de543d7948119ada99744a
guta231 <gustavoho2005@gmail.com>
2024-04-22 22:21:30 -0300

    adicionado barra de navegação fixa e nova imagem para projeto luminosidade

---
+++ Projeto-3-main/script/index.js
@@ -0,0 +1,15 @@
+function projects(){
+    var projects = document.querySelector(".section2");
+
+    projects.scrollIntoView({behavior: "smooth"});
+}
+function contact(){
+    var contact = document.querySelector(".contatos");
+
+    contact.scrollIntoView({behavior: "smooth"});
+}
+function sobre(){
+    
+
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
